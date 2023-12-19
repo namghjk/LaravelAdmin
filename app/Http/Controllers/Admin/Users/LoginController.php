3bca72ee1fd37b32a5405e4a81ee6ca2a5e6fb3d
Nam Pham <namsgu123@gmail.com>
2023-12-19 11:08:53 +0700

    add seed & logout button

--- app/Http/Controllers/Admin/Users/LoginController.php
+++ app/Http/Controllers/Admin/Users/LoginController.php
@@ -38,6 +38,6 @@ class LoginController extends Controller
     Auth::guard('web')->logout();
     $request->session()->invalidate();
     $request->session()->regenerateToken();
-    return redirect()->route('admin');
+    return redirect()->route('admin'); 
   }
 }
